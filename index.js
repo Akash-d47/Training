bdac532c5e51bb06e02f126c119fc30e565c96da
Akash <Akashsinghd47@gmail.com>
2024-10-03 12:29:09 +0530

    My first Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const operator=prompt("Enter Operand (+,-,*,/)");
+const sum(a=10,b=20)
+{
+    switch(operator)
+    {
+        case '+':
+            console.log(a+b);
+            break;
+        case '-':
+            console.log(a-b);
+            break;
+        case '*':
+            console.log(a*b);
+            break;
+        case '/':
+            console.log(a/b);
+            break;
+        default:
+            console.log("Error!");                
+
+    }
+}
